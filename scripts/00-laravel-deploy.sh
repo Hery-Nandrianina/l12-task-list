062cade06c979bc12807a7713fc8bb1a2a1ebe24
Hery-Nandrianina <earnandrianina@gmail.com>
2025-06-17 10:02:09 +0300

    Deploy Fix

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -13,7 +13,7 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate:fresh --force
 
 echo "Running seed..."
 php artisan db:seed
